47cd2a7916d91b133dafe386fe8c4ef9cd952843
Automated Publisher <actions@users.noreply.github.com>
2022-01-12 02:40:38 +0000

    Automated publish: Wed Jan 12 02:40:38 UTC 2022 cab2b599b658f2046649c8842814d0a9eef9b747

--- tokens/colors.d.ts
+++ tokens/colors.d.ts
@@ -1,15 +1,31 @@
 /**
  * Do not edit directly
- * Generated on Wed, 12 Jan 2022 00:40:59 GMT
+ * Generated on Wed, 12 Jan 2022 02:40:37 GMT
  */
 
-export const colorColorsBlue100 : string;
-export const colorColorsBlue300 : string;
-export const colorColorsBlue500 : string;
-export const colorColorsBlue700 : string;
-export const colorColorsBlue900 : string;
-export const colorColorsGreen100 : string;
-export const colorColorsGreen300 : string;
-export const colorColorsGreen500 : string;
-export const colorColorsGreen700 : string;
-export const colorColorsGreen900 : string;
+export const blue100 : string;
+export const blue300 : string;
+export const blue500 : string;
+export const blue700 : string;
+export const blue900 : string;
+export const green100 : string;
+export const green300 : string;
+export const green500 : string;
+export const green700 : string;
+export const green900 : string;
+export const yellow100 : string;
+export const yellow300 : string;
+export const yellow500 : string;
+export const yellow700 : string;
+export const yellow900 : string;
+export const red100 : string;
+export const red300 : string;
+export const red500 : string;
+export const red700 : string;
+export const red900 : string;
+export const greyscale100 : string;
+export const greyscale300 : string;
+export const greyscale500 : string;
+export const greyscale700 : string;
+export const greyscale900 : string;
+export const greyscale1100 : string;
--- tokens/colors.ts
+++ tokens/colors.ts
@@ -1,15 +1,31 @@
 /**
  * Do not edit directly
- * Generated on Wed, 12 Jan 2022 00:40:59 GMT
+ * Generated on Wed, 12 Jan 2022 02:40:37 GMT
  */
 
-export const colorColorsBlue100 = "#e6eef6ff";
-export const colorColorsBlue300 = "#ccddedff";
-export const colorColorsBlue500 = "#ff54a4ff";
-export const colorColorsBlue700 = "#1b4774ff";
-export const colorColorsBlue900 = "#0d2135ff";
-export const colorColorsGreen100 = "#e7f5f1ff";
-export const colorColorsGreen300 = "#d0ece2ff";
-export const colorColorsGreen500 = "#139f6fff";
-export const colorColorsGreen700 = "#0e7753ff";
-export const colorColorsGreen900 = "#0a5038ff";
+export const blue100 = "#e6eef6ff";
+export const blue300 = "#ccddedff";
+export const blue500 = "#ff54a4ff";
+export const blue700 = "#1b4774ff";
+export const blue900 = "#0d2135ff";
+export const green100 = "#e7f5f1ff";
+export const green300 = "#d0ece2ff";
+export const green500 = "#139f6fff";
+export const green700 = "#0e7753ff";
+export const green900 = "#0a5038ff";
+export const yellow100 = "#fff8ebff";
+export const yellow300 = "#fff1d6ff";
+export const yellow500 = "#ffb900ff";
+export const yellow700 = "#e5a7ffff";
+export const yellow900 = "#926bffff";
+export const red100 = "#fee6daff";
+export const red300 = "#fec7b5ff";
+export const red500 = "#f84647ff";
+export const red700 = "#c9393aff";
+export const red900 = "#7a2223ff";
+export const greyscale100 = "#ffffffff";
+export const greyscale300 = "#f4f6f9ff";
+export const greyscale500 = "#e7e9ebff";
+export const greyscale700 = "#b6bcc2ff";
+export const greyscale900 = "#86909aff";
+export const greyscale1100 = "#0d2135ff";
