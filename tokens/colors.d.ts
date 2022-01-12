87bb7e81c34ffca94baacc1e0969146e7e04855f
Automated Publisher <actions@users.noreply.github.com>
2022-01-12 00:41:01 +0000

    Automated publish: Wed Jan 12 00:41:01 UTC 2022 cbfb4c459314b7977f71e3b8d21f8aae2c7fa756

---
+++ tokens/colors.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 12 Jan 2022 00:40:59 GMT
+ */
+
+export const colorColorsBlue100 : string;
+export const colorColorsBlue300 : string;
+export const colorColorsBlue500 : string;
+export const colorColorsBlue700 : string;
+export const colorColorsBlue900 : string;
+export const colorColorsGreen100 : string;
+export const colorColorsGreen300 : string;
+export const colorColorsGreen500 : string;
+export const colorColorsGreen700 : string;
+export const colorColorsGreen900 : string;
---
+++ tokens/colors.ts
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 12 Jan 2022 00:40:59 GMT
+ */
+
+export const colorColorsBlue100 = "#e6eef6ff";
+export const colorColorsBlue300 = "#ccddedff";
+export const colorColorsBlue500 = "#ff54a4ff";
+export const colorColorsBlue700 = "#1b4774ff";
+export const colorColorsBlue900 = "#0d2135ff";
+export const colorColorsGreen100 = "#e7f5f1ff";
+export const colorColorsGreen300 = "#d0ece2ff";
+export const colorColorsGreen500 = "#139f6fff";
+export const colorColorsGreen700 = "#0e7753ff";
+export const colorColorsGreen900 = "#0a5038ff";
