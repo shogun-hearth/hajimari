e68d1e79a00270c0b4ff7b35de4c15b98f4acdf3
Vernon <iseeyoulater123@gmail.com>
2022-03-29 16:36:23 -0400

    template

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Switch from '@mui/material/Switch';
+
+// export type SwitchProps = {
+//     enabled: boolean,
+//     selected: Boolean,
+// };
+
+const Switch = <div>Switch</div>;
+
+export default Switch;
