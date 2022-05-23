b99e2132a7d3d3745ed6bd6599b1eca2add14c25
Alex Wolinsky <alex@gethearth.com>
2022-05-23 09:36:47 -0700

    build accordion component and basic use case

---
+++ src/components/SuggestedActionAccordion/SuggestedActionAccordion.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import Accordion, { AccordionProps } from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Brightness1Icon from '@mui/icons-material/Brightness1';
+
+import Box from '../Box';
+import Typography from '../Typography';
+
+type SuggestedActionAccordionVariant = 'green' | 'yellow' | 'red' | 'greyscale';
+
+export interface SuggestedActionAccordionProps extends Omit<AccordionProps, 'variant'> {
+  /* sets color of left pill */
+  variant: SuggestedActionAccordionVariant;
+  /* title of accordion header */
+  groupTitle: string;
+  /* number of suggested actions in category */
+  numItems: number;
+}
+
+const SuggestedActionAccordion = ({
+  variant,
+  groupTitle,
+  numItems,
+  children,
+  ...otherProps
+}: SuggestedActionAccordionProps): JSX.Element => {
+  const setIconColor = (variant: SuggestedActionAccordionVariant): HajimariColor => {
+    switch(variant) {
+    case 'green':
+      return 'green.500';
+    case 'yellow':
+      return 'yellow.500';
+    case 'red':
+      return 'red.500';
+    case 'greyscale':
+      return 'greyscale.700';
+    }
+  };
+
+  return (
+    <Accordion {...otherProps}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon sx={{ fontSize: 24 }} />}
+        sx={{
+          p: 2,
+          boxShadow: 'none',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flex: 1,
+            mr: 1,
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Brightness1Icon sx={{ color: setIconColor(variant), fontSize: 10, mr: 1.5 }} />
+            <Typography variant="p1" weight="semibold">
+              {groupTitle}
+            </Typography>
+          </Box>
+          <Typography variant="p1">
+            {numItems}
+          </Typography>
+        </Box>
+      </AccordionSummary>
+      <AccordionDetails>
+        {children}
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default SuggestedActionAccordion;
