66de042f45cfcab8bb40528e64dfae65891fe7fe
eynglv <73679611+eynglv@users.noreply.github.com>
2022-11-18 14:55:18 -0800

    [KOJI-123] Data Grid (#69)
    * pause
    
    * start datagrid
    
    * basic datagrid
    
    * clean up
    
    * ?
    
    * clean up + words
    
    * revert file
    
    * add arguments table
    
    * add another story
    
    * clean clean
    
    * comments
    
    * rollup
    
    * column header style thing
    
    * nit
    
    * props
    
    * rollup

---
+++ src/components/DataGrid/DataGrid.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  DataGrid as MuiDataGrid,
+  DataGridProps as MuiDataGridProps,
+} from '@mui/x-data-grid';
+
+import theme from '../../theme';
+import Box from '../Box';
+import styled from '../../theme/styled';
+
+export interface DataGridProps extends MuiDataGridProps {
+  /**
+   * the number of rows dsiplayed per page
+   *
+   * @optional
+   * @default 10
+   * */
+  pageSize?: number;
+  /**
+   * sets the height of the table. Mui seems to accept units like px and vh but not %
+   *
+   * @default "500px"
+   * */
+  height: string;
+}
+
+const DataGridRoot = styled(MuiDataGrid)(({ theme }) => ({
+  '&.MuiDataGrid-root .MuiDataGrid-cell:focus': {
+    outline: 'none',
+  },
+  '&.MuiDataGrid-root .MuiDataGrid-columnHeader:focus': {
+    outline: 'none',
+  },
+  '&.MuiDataGrid-root .MuiDataGrid-columnHeaders': {
+    borderBottom: `1px solid ${theme.palette.greyscale[700]}`,
+  },
+}));
+
+const Datagrid = ({
+  columns,
+  pageSize = 10,
+  rows,
+  height = '500px',
+  ...otherProps
+}: DataGridProps): JSX.Element => (
+  <Box sx={{ height: height, width: '100%' }}>
+    <DataGridRoot
+      rows={rows}
+      columns={columns}
+      pageSize={pageSize}
+      disableColumnSelector
+      disableDensitySelector
+      hideFooterSelectedRowCount
+      showColumnRightBorder={false}
+      rowHeight={65}
+      sx={{
+        backgroundColor: theme.palette.greyscale[100],
+        border: 'none',
+      }}
+      {...otherProps}
+    />
+  </Box>
+);
+
+export default Datagrid;
