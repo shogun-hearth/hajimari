983cd9fd1a8b2835985a0faba0b4d680bc8f2662
Stefano Chiesa <stefano@shoguninc.com>
2022-12-20 22:21:55 +0700

    asdf

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,10 +1,5 @@
-import React, { forwardRef } from 'react';
 import MuiBox, { BoxProps as MuiBoxProps } from '@mui/material/Box';
 
 export interface BoxProps extends MuiBoxProps { };
 
-const Box = forwardRef((props: BoxProps, ref): JSX.Element => (
-  <MuiBox {...props} ref={ref} />
-));
-
-export default Box;
+export default MuiBox;
